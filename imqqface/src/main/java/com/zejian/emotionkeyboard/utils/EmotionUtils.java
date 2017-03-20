1ca24b1eecf192acacbac9fbf2f4900f6cf428fb
Jony Huang <jonyhuang@shijiebang.net>
2017-03-20 11:39:44 +0800

    QQ表情面板

---
+++ imqqface/src/main/java/com/zejian/emotionkeyboard/utils/EmotionUtils.java
@@ -0,0 +1,76 @@
+
+package com.zejian.emotionkeyboard.utils;
+
+import android.content.Context;
+import java.util.LinkedHashMap;
+
+
+/**
+ * @author : zejian
+ * @time : 2016年1月5日 上午11:32:33
+ * @email : shinezejian@163.com
+ * @description :表情加载类,可自己添加多种表情，分别建立不同的map存放和不同的标志符即可
+ */
+public class EmotionUtils {
+
+	/**
+	 * 表情类型标志符
+	 */
+	public static final int EMOTION_CLASSIC_TYPE=0x0001;//经典表情
+
+	/**
+	 * key-表情文字;
+	 * value-表情图片资源
+	 */
+	public static LinkedHashMap<String, Integer> EMPTY_MAP;
+	public static LinkedHashMap<String, Integer> EMOTION_CLASSIC_MAP;
+
+
+	static final String[] emojiNameList = {"","微笑","撇嘴","色","发呆","得意","流泪","害羞","闭嘴","睡","大哭","尴尬","发怒","调皮","呲牙","惊讶","难过","酷","冷汗","抓狂","吐","偷笑","可爱","白眼","傲慢","饥饿","困","惊恐","流汗","憨笑","大兵","奋斗","咒骂","疑问","嘘","晕","折磨","衰","骷髅","敲打","再见","擦汗","抠鼻","鼓掌","糗大了","坏笑","左哼哼","右哼哼","哈欠","鄙视","委屈","快哭了","阴险","亲亲","吓","可怜","菜刀","西瓜","啤酒","篮球","乒乓","咖啡","饭","猪头","玫瑰","凋谢","示爱","爱心","心碎","蛋糕","闪电","炸弹","刀","足球","瓢虫","便便","月亮","太阳","礼物","拥抱","强","弱","握手","胜利","抱拳","勾引","拳头","差劲","爱你","NO","OK","爱情","飞吻","跳跳","发抖","怄火","转圈","磕头","回头","跳绳","挥手"};
+
+	public static void init(Context context){
+		EMPTY_MAP = new LinkedHashMap<>();
+		EMOTION_CLASSIC_MAP = new LinkedHashMap<>();
+		for (int x = 1; x <= 100; x++) {
+			int identifier = context.getResources().getIdentifier("bq" + x, "drawable", context.getPackageName());
+			EMOTION_CLASSIC_MAP.put(emojiNameList[x], identifier);
+		}
+	}
+
+	/**
+	 * 根据名称获取当前表情图标R值
+	 * @param EmotionType 表情类型标志符
+	 * @param imgName 名称
+	 * @return
+	 */
+	public static int getImgByName(int EmotionType,String imgName) {
+		Integer integer=null;
+		switch (EmotionType){
+			case EMOTION_CLASSIC_TYPE:
+				integer = EMOTION_CLASSIC_MAP.get(imgName);
+				break;
+			default:
+				LogUtils.e("the emojiMap is null!! Handle Yourself ");
+				break;
+		}
+		return integer == null ? -1 : integer;
+	}
+
+	/**
+	 * 根据类型获取表情数据
+	 * @param EmotionType
+	 * @return
+	 */
+	public static LinkedHashMap<String, Integer> getEmojiMap(int EmotionType){
+		LinkedHashMap EmojiMap=null;
+		switch (EmotionType){
+			case EMOTION_CLASSIC_TYPE:
+				EmojiMap=EMOTION_CLASSIC_MAP;
+				break;
+			default:
+				EmojiMap=EMPTY_MAP;
+				break;
+		}
+		return EmojiMap;
+	}
+}
